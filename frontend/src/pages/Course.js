b9f843b55b6b1a8aa63bc43df4ff9718eb83f709
MGhazouly <96923712+MGhazouly@users.noreply.github.com>
2022-10-23 23:16:23 +0200

    merge b frontend bayza

--- frontend/src/pages/Course.js
+++ frontend/src/pages/Course.js
@@ -1,11 +1,12 @@
-import Navbar from "../components/NavBar"
+import Navbar from "../components/navBar"
 
-export default function Course(){
-    return(
-           <div className="coursePage">
-            <Navbar/>
-            <h1>Course Page</h1>
-           </div>
-       )
+export default function Course() {
+    return ( <
+        div className = "coursePage" >
+        <
+        Navbar / >
+        <
+        h1 > Course Page < /h1> <
+        /div>
+    )
 }
-   
