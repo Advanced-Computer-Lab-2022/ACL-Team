e81df7dc9d0302b5feda1c37106c9c04d9a6eec7
MGhazouly <96923712+MGhazouly@users.noreply.github.com>
2022-10-26 11:38:23 +0200

    authentication final1

--- BackEnd/models/AdminSchema.js
+++ BackEnd/models/AdminSchema.js
@@ -16,13 +16,19 @@ const AdminSchema = new Schema({
     
     gender: {
         type: String
+    },
+    email: {
+        type: String
+    },
+    name: {
+        type: String
     }
 
 }, { timestamps: true })
-AdminSchema.statics.signup = async function(username , password , gender){
+AdminSchema.statics.signup = async function(name,email,username,password,gender){
     const usernameExists = await this.findOne({username})
 
-    if(!username || !password || gender)
+    if(!username || !password)
         throw error('All fields must be filled')
     if(usernameExists)
         throw Error('Username already in use')
@@ -31,11 +37,35 @@ AdminSchema.statics.signup = async function(username , password , gender){
     const salt = await bcrypt.genSalt(10)
     const hash = await bcrypt.hash(password,salt) 
     
-    const admin = await this.create({username , password: hash , gender})
+    const admin = await this.create({username , password: hash })
 
     return admin
 }
 
+AdminSchema.statics.login = async function(email, password){
+     
+    if(!email || !password)
+        throw Error('All fields must be filled')
+
+    const admin = await this.findOne({ email })
+    if(!admin)
+        throw Error('Incorrect email')
+   
+
+    const match = await bcrypt.compare(password, admin.password)
+
+    if(!match)
+        throw Error('Incorrect password')
+    
+    return admin
+}
+
+AdminSchema.statics.emailExists = async function(email){
+    return await this.findOne({email})
+}
+AdminSchema.statics.usernameExists = async function(username){
+    return await this.findOne({email})
+}
 
 AdminSchema.index({id:'text'})
 module.exports = mongoose.model('Admin', AdminSchema)
