a89df4863238e589eadebaf7a158dd548db259c7
Mazen317 <mazen21hejazy@gmail.com>
2022-10-28 13:39:20 +0200

    "new

--- BackEnd/models/userSchema.js
+++ BackEnd/models/userSchema.js
@@ -1,100 +1,59 @@
-const mongoose = require('mongoose')
-const bcrypt=require('bcrypt')
-const validator = require('validator')
-const Schema = mongoose.Schema
+import mongoose from 'mongoose'
+import crypto from 'crypto'
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: 'Name is required'
+  },
+  email: {
+    type: String,
+    trim: true,
+    unique: 'Email already exists',
+    match: [/.+\@.+\..+/, 'Please fill a valid email address'],
+    required: 'Email is required'
+  },
 
+})
 
-const UserSchema = new Schema({
-    
-    isCorporate: {
-        type: Boolean,
-        required: true
-    },
-    username: {
-        type: String,
-        required: 'Username is required',
-        unique:true
+UserSchema
+  .virtual('password')
+  .set(function(password) {
+    this._password = password
+    this.salt = this.makeSalt()
+    this.hashed_password = this.encryptPassword(password)
+  })
+  .get(function() {
+    return this._password
+  })
 
-    },
-    email: {
-        type: String,
-        required: 'Email is required',
-        unique:true,
-        match: [/.+\@.+\..+/, 'Please fill a valid email address']
+UserSchema.path('hashed_password').validate(function(v) {
+  if (this._password && this._password.length < 6) {
+    this.invalidate('password', 'Password must be at least 6 characters.')
+  }
+  if (this.isNew && !this._password) {
+    this.invalidate('password', 'Password is required')
+  }
+}, null)
 
-
-        //had y3ml regex lel email
-    },
-     password: {
-        type: String,
-        required: true
-    },
-    gender: {
-        type: String,
-
-    },
-    boughtCourses: {
-        type: String,
-
-    },
-    country: {
-        type: String,
-
-    },
-    creditCardDetails: {
-        type: String,
-
-    },
-
-    //lesa fee ba2y
-}, { timestamps: true })
-
-UserSchema.statics.signup=async function(email,username,password,isCorporate){
-    const emailExists =await this.findOne({email})
-    const usernameExists =await this.findOne({username})
-
-    if(!email || !password)
-        throw Error('All fields must be filled')
-    if (emailExists)
-        throw Error('Email already in use')
-    if (usernameExists)
-        throw Error('Username already in use')
-    if(!validator.isEmail(email))
-        throw Error('Email is not valid')
-    // if(!validator.isStrongPassword(password))
-    //     throw Error('Email is not valid')
-
-        
-    const salt=await bcrypt.genSalt(10)
-    const hash=await bcrypt.hash(password,salt)
-
-    const user=await this.create({email,username,password: hash, isCorporate})
-
-    return user  
+UserSchema.methods = {
+  authenticate: function(plainText) {
+    return this.encryptPassword(plainText) === this.hashed_password
+  },
+  encryptPassword: function(password) {
+    if (!password) return ''
+    try {
+      return crypto
+        .createHmac('sha1', this.salt)
+        .update(password)
+        .digest('hex')
+    } catch (err) {
+      return ''
+    }
+  },
+  makeSalt: function() {
+    return Math.round((new Date().valueOf() * Math.random())) + ''
+  }
 }
 
-UserSchema.statics.login = async function(email,password) {
-    
-    if(!email || !password)
-        throw Error('All fields must be filled')
-
-    const user = await this.findOne({ email })
-    if(!user)
-        throw Error('Incorrect email')
-   
-
-    const match = await bcrypt.compare(password, user.password)
-
-    if(!match)
-        throw Error('Incorrect password')
-    
-    return user
-
-}
-
-
-
-
-
-
-module.exports = mongoose.model('User', UserSchema)
+export default mongoose.model('User', UserSchema)
