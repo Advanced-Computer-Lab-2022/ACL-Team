ba0c2351bbc195589d4420e3b7e968a73f5913b5
MGhazouly <96923712+MGhazouly@users.noreply.github.com>
2022-11-04 17:37:23 +0200

    hi

--- BackEnd/models/AdminSchema.js
+++ BackEnd/models/AdminSchema.js
@@ -5,13 +5,31 @@ const Schema = mongoose.Schema
 const AdminSchema = new Schema({
     _id: {
         type: mongoose.Schema.Types.ObjectId,
-         ref: 'User',
-         required: true
+        ref: 'User',
+        required: true
 
     },
-    
-}, { timestamps: true })
+
+}, {
+    timestamps: true
+})
+
+AdminSchema.statics.signup = async function (email, username, password, firstname, lastname, gender) {
+    const user = await User.signup(email, username, password, firstname, lastname, gender)
+
+    User.findByIdAndUpdate({
+        _id: user._id
+    }, {
+
+        role: "instructor"
+    })
+
+    const admin = await this.create({
+        _id: user._id,
+    })
+
+    return admin
+
+}
 
 module.exports = mongoose.model('Admin', AdminSchema)
-
-
