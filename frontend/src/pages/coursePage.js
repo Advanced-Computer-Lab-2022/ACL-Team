8a78c3045fd1a73212390159fe4708e16f65457d
marwanashraf56 <marwan.ashrafshaaban123@gmail.com>
2022-11-25 03:03:25 +0200

    course page frontend done

--- frontend/src/pages/coursePage.js
+++ frontend/src/pages/coursePage.js
@@ -1,11 +1,23 @@
 import React from 'react'
+import BuyCourseCard from '../Components/Cards/buyCourseCard'
+import Rating from '../Components/Cards/rating'
+import RatingsCard from '../Components/Course/ratingsCard'
+import Navbar from '../Components/General/Navbar/navbar'
 
 export const CoursePage = () => {
   return (
     <div>
-        <h3>
-            Course Page hiii
-        </h3>
+      <Navbar/>
+      <div>
+        <RatingsCard/>
+      </div>
+      <div>
+        <BuyCourseCard/>
+      </div>
+      <div>
+        
+      </div>
     </div>
+    
   )
 }
--- frontend/src/pages/instructorAddQuizPage.js
+++ frontend/src/pages/instructorAddQuizPage.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import QuestionCard from '../Components/Cards/questionCard'
 import QuizTitle from '../Components/Cards/quizTitle'
-import Navbar from '../Components/General/navbar'
+import Navbar from '../Components/General/Navbar/navbar'
 import QuestionsComponents from '../Components/Instructor/questionsComponents'
 import "./instructorAddQuizPage.css"
 
