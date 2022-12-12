b40728ef21c5a69dbac851ce063004f0bc77b06f
Sherif Hejazy <mazen21hejazy@gmail.com>
2022-12-12 18:08:41 +0200

    Fixed model answer Page

--- frontend/src/Components/Cards/quizResultCard.js
+++ frontend/src/Components/Cards/quizResultCard.js
@@ -4,6 +4,8 @@ import img1 from "../images/Blogging-bro 1.png"
 
 export default function QuizResultCard({question}) {
 
+  const questionAnswer = question.answer;
+
   return (
     <div className='border'> 
     <div className='course-details'>
@@ -14,9 +16,14 @@ export default function QuizResultCard({question}) {
 
 <div className="course-text">
     <h4>{question.question}</h4>
+
+     {questionAnswer == "choice_1" && <h6>Correct Answer: {question.choices[0].choice_1}</h6>}
+     {questionAnswer == "choice_2" && <h6>Correct Answer: {question.choices[0].choice_2}</h6>}
+     {questionAnswer == "choice_3" && <h6>Correct Answer: {question.choices[0].choice_3}</h6>}
+     {questionAnswer == "choice_4" && <h6>Correct Answer: {question.choices[0].choice_4}</h6>}
     
 
-  <h6>Correct Answer: {question.answer}</h6>
+  {/* <h6>Correct Answer: {(question.choices[0]) + "." + `${questionAnswer}`}</h6> */}
 
 </div>
 
